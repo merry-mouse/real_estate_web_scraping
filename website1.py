9b32b0dbad5ffeacba52e1cf0c1ba555dbb6ac4b
merry-mouse <80622089+merry-mouse@users.noreply.github.com>
2021-11-17 11:11:39 +0100

    Combining scraping download photos (#1)
    * Combined 2 codes together now this program can also download a stack of photos from each apartment to the separate new folder (0,1,2... according to the link numerate in the database) to the "APARTMENTS" folder
    
    * Adding new comments that explain program behavior
    
    * Adding final comments

--- website1.py
+++ website1.py
@@ -15,14 +15,14 @@ print("Openning browser...\n\n")
 
 # initialize automated browser window, specify the browser we want to use
 # store the path of the driver file in the brackets
-driver = webdriver.Chrome("C:/zadachki/Different_Upwork_Projects/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome("C:/Users/potek/Jupyter_projects/chromedriver_win32/chromedriver.exe")
 # open the url you would like to request
-driver.get("https://en.comparis.ch/immobilien/result/list?requestobject=%7B%22DealType%22%3A10%2C%22SiteId%22%3A-1%2C%22RootPropertyTypes%22%3A%5B%5D%2C%22PropertyTypes%22%3Anull%2C%22RoomsFrom%22%3A%22-10%22%2C%22RoomsTo%22%3Anull%2C%22FloorSearchType%22%3A0%2C%22LivingSpaceFrom%22%3Anull%2C%22LivingSpaceTo%22%3Anull%2C%22PriceFrom%22%3Anull%2C%22PriceTo%22%3A%22-10%22%2C%22ComparisPointsMin%22%3A-1%2C%22AdAgeMax%22%3A-1%2C%22AdAgeInHoursMax%22%3Anull%2C%22Keyword%22%3Anull%2C%22WithImagesOnly%22%3Anull%2C%22WithPointsOnly%22%3Anull%2C%22Radius%22%3Anull%2C%22MinAvailableDate%22%3Anull%2C%22MinChangeDate%22%3Anull%2C%22LocationSearchString%22%3A%22Geneve%22%2C%22Sort%22%3A11%2C%22HasBalcony%22%3Afalse%2C%22HasTerrace%22%3Afalse%2C%22HasFireplace%22%3Afalse%2C%22HasDishwasher%22%3Afalse%2C%22HasWashingMachine%22%3Afalse%2C%22HasLift%22%3Afalse%2C%22HasParking%22%3Afalse%2C%22PetsAllowed%22%3Afalse%2C%22MinergieCertified%22%3Afalse%2C%22WheelchairAccessible%22%3Afalse%2C%22LowerLeftLatitude%22%3Anull%2C%22LowerLeftLongitude%22%3Anull%2C%22UpperRightLatitude%22%3Anull%2C%22UpperRightLongitude%22%3Anull%7D&page=0")
-time.sleep(2)
+driver.get("https://en.comparis.ch/immobilien/marktplatz/luzern/mieten")
+time.sleep(0.5)
 
 
 # scraping the number of hits found
-# we need this number if want to scrape links from all pages, specify it in range() in the next block
+# we need it ONLY if want to scrape links from all pages, specify it in range() in the next block
 print("Checking how many apartments there are on a website...\n\n")
 num_hits_raw = driver.find_element_by_xpath('//html/body/div/div/div/div/div/div/div/div/div/div/p/strong').get_attribute("innerText") # long comment
 num_hits = int(re.match("(^\d*(?:\,)\d*)", num_hits_raw).group(0).replace(",", ""))
@@ -32,18 +32,21 @@ print("There are {} items found at the website.\n\n".format(num_hits))
 # scraping all the links and saving them into "links" list
 last_page = int(num_hits/10+2)
 links = []
-# here I am scraping only from 5 pages (number of pages needed +2)
+# here I am scraping only from 2 pages (20 apartments)
 # to scrape all, change the second number in range() to last_page
-for i in range(2, 7):
+for i in range(2, 4):
     try:
+        # takes time for page to upload, must scroll down to be able to scrape all info
         driver.execute_script("window.scrollTo(0, 1900);")
         time.sleep(1)
         driver.execute_script("window.scrollTo(0, window.scrollY + 1100)")
         time.sleep(1)
+        # scraping apartments' containers and links to them inside
         containers = driver.find_elements_by_class_name("css-ctytwt.excbu0j5")
         for container in containers:
             link = container.find_element_by_css_selector("a").get_attribute("href")
             links.append(link)
+        # click to the next page
         driver.find_element_by_xpath(("//a[text()='{}']").format(i)).click()
         time.sleep(2)
     except NoSuchElementException:
@@ -58,10 +61,11 @@ found_on = []
 key_data = []
 descriptions = []
 print("Scraping key info from each link...\n\n")
-for link in links:
+for num, link in enumerate(links):
+    # going through each apartment
     url = link
     driver.get(url)
-    # Scraping ADDRESS (map)
+    # Scraping ADDRESS (one that's above the map)
     try:
         address1 = driver.find_element_by_css_selector("h5").get_attribute("innerText")
         address.append(address1)
@@ -75,7 +79,8 @@ for link in links:
     except NoSuchElementException:
         agent.append("NO AGENT INFO")
 
-    # Scraping FOUND ON
+    # Scraping FOUND ON which represents names of websites where apartment was found
+    # requires 2 steps, sometimes there are more than 1 website
     # step 1
     try:
         found1 = [
@@ -96,7 +101,7 @@ for link in links:
     hdrs = driver.find_elements_by_class_name("css-cyiock.excbu0j2")
     undrhdrs = driver.find_elements_by_class_name("css-1ush3w6.excbu0j2")
     keyd_dict = {k.get_attribute("innerText"): v.get_attribute("innerText") for k, v in zip(hdrs, undrhdrs)}
-    # changing empty values to "Available"
+    # changing empty values to "Available" since instead of text there can be check marks
     empty = ""
     # initializing replace value
     repl_val = "Available"
@@ -113,7 +118,37 @@ for link in links:
         descriptions.append(description)
     except NoSuchElementException:
         descriptions.append(["No description found."])
-    time.sleep(2)
+
+    # COUNT NUMBER OF SMALL CIRCLES THAT REPRESENTS NUMBER OF PHOTOS
+    # add 1 if you want your code inform you about the number of found photos (for debugging)
+    circles = len(driver.find_elements_by_class_name("svg-inline--fa.fa-circle.fa-w-16.css-1xkwzfp"))
+    print("for apartment {}, {} photos found at the website.".format(num, circles + 1))
+    # CREATE A SET TO AVOID DUPLICATES
+    images_urls = set()
+    for n in range(circles):
+        # FIND IMAGE CONTAINERS
+        images_containers = driver.find_element_by_class_name("css-ze3zoq").find_elements_by_tag_name("img")
+        for image in images_containers:
+            # FIND IMAGES' URLS, STORE THEM IN A SET
+            images_urls.update([image.get_attribute("src")])
+            # FIND BUTTON THAT SCROLL TO THE RIGHT AND CLICK IT
+            driver.find_element_by_class_name("css-11m3oda.excbu0j2").click()
+            time.sleep(0.2)
+
+    # DOWNLOAD PHOTOS IN "APARTMENTS" AND CREATE NEW FOLDER FOR EACH APARTMENT
+    # replace path with the directory where you want to store photos
+    for i in images_urls:
+        path = "C:/Users/potek/Jupyter_projects/APARTMENTS/{}".format(num)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        with open(os.path.join(path, "Comparis" + str(time.time()) + ".jpg"), "wb") as f:
+            try:
+                # DOWNLOAD CONTENT OF THE URL
+                f.write(requests.get(i).content)
+            except:
+                pass
+    time.sleep(0.2)
+
 
 # saving scraped info into a single dataframe
 print("Saving scraped info into a dataframe and .csv\n\n")
