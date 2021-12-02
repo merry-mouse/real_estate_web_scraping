daafcd74a33b9dcbc92534710f9bb99b55c46e5d
merry-mouse <80622089+merry-mouse@users.noreply.github.com>
2021-12-02 11:57:57 +0100

    changing waiting time
    No need to wait for 4 seconds to close cookies tab, 2 sec is enough

--- website3.py
+++ website3.py
@@ -15,6 +15,6 @@ print("Openning browser...\n\n")
 driver = webdriver.Chrome("C:/Users/potek/Jupyter_projects/chromedriver_win32/chromedriver.exe")
 # open the url you would like to request
 driver.get("https://www.anibis.ch/fr/c/immobilier-immobilier-locations?sct=GE")#comment
-time.sleep(4)
+time.sleep(2)
 # close cookies tab
 driver.find_element_by_class_name("cmp-closebutton_closeButton.cmp-closebutton_hasBorder").click()
