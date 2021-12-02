3efab50c7fc311bddf616df7b58bb0e59a4dc087
anastasiia <potekh.anastasia@gmail.com>
2021-12-02 12:44:32 +0100

    Added the forth block of code which scraping info from each link and downloads photos

--- website3.py
+++ website3.py
@@ -43,3 +43,109 @@ for i in range(2, 4):
     # clicks to the next page
     driver.find_element_by_xpath("//div[text()='{}']".format(i)).click()
     time.sleep(2)
+
+# SCRAPING MAIN INFO FROM EACH LINK
+headers = []
+details = []
+descriptions = []
+addresses = []
+announcers = []
+announces = []
+tels = []
+for num, link in enumerate(links):
+    driver.get(link)
+    # HEADERS
+    try:
+        header = driver.find_element_by_class_name("cahxkj-0.sc-1645p4v-0.brwHqV.edLfCV").get_attribute("innerText")
+        headers.append(header.replace("\n", ", "))
+    except NoSuchElementException:
+        headers.append("No header found")
+    # DETAILS (list of dicts)
+    try:
+        detail = (driver.find_element_by_class_name("sc-79mv0k-0.cthnIw").get_attribute("innerText")).split(
+            "\n")  # comme
+        details.append(dict(zip(detail[::2], detail[1::2])))
+    except NoSuchElementException:
+        details.append("No details found")
+
+    # DESCRIPTIONS
+    try:
+        description = driver.find_element_by_class_name("urtii3-0.xVgjA").get_attribute("innerText").replace("\n", ", ")
+        descriptions.append(description)
+    except NoSuchElementException:
+        descriptions.append("No description found")
+    # ADDRESSES
+    try:
+        address = driver.find_element_by_class_name("sc-1oudagz-0.sc-1f77bma-0.covveV.bylUyk").get_attribute(
+            "innerText").replace("\n", ", ")  # comment
+        addresses.append(address)
+    except NoSuchElementException:
+        addresses.append("No address found")
+    # ANNOUNCER
+    try:
+        announcer = driver.find_element_by_class_name("sc-gsTCUz.iqdsSd").get_attribute("innerText")
+        announcers.append(announcer)
+    except NoSuchElementException:
+        announcers.append("No announcer info")
+    # PHONE NUMBER
+    try:
+        tel = driver.find_element_by_class_name("sc-1nm2xjp-0.jJCcfn").find_element_by_tag_name("a").get_attribute(
+            "innerText")  # comment
+        tels.append(tel)
+    except NoSuchElementException:
+        tels.append("No contact number found")
+    # ANNOUNCE
+    try:
+        announce = driver.find_element_by_class_name("sc-1daxexx-0.fPZErg").get_attribute("innerText").split("\n")
+        a = dict(zip(announce[::2], announce[1::2]))
+        b = a.pop("Signaler une annonce suspecte")
+        announces.append(a)
+    except NoSuchElementException:
+        announces.append("No announce found")
+    # DOWNLOAD PICTURES
+    try:
+        # find number of circles(photos) at the page
+        circles = driver.find_elements_by_class_name("v6h5f1-0.jvMQcP")
+        # find swiping button
+        button = driver.find_element_by_class_name("cahxkj-0.nw116f-0.kyWlQc.jOqXyv.swiper-button-prev")
+        # we can oly scrape 5 urls, for more urls we have to scroll ptotos to the right|left
+        number_of_clicks = (len(circles) + 1 - 5)
+        if number_of_clicks > 0:
+            for i in range(number_of_clicks):
+                button.click()
+                time.sleep(0.3)
+    # sometimes there is just 1 photo and thus, 0 circles
+    except NoSuchElementException:
+        pass
+    # creating a set where we store urls for pics if there are pics
+    try:
+        images_urls = set()
+        img_containers = driver.find_element_by_class_name(
+            "vs5092-0.bJHlto.swiper-container.swiper-uid-catalog-gallery").find_elements_by_tag_name("img")
+        for img in img_containers:
+            try:
+                images_urls.update([img.get_attribute("src")])
+            except NoSuchElementException:
+                pass
+        print("For apartment {}, {} photos found at the website.".format(num, len(images_urls)))
+        # download photos in the same folder
+        for i in images_urls:
+            path = "C:/Users/potek/Jupyter_projects/APARTMENTS/{}".format(num)
+            if not os.path.exists(path):
+                os.mkdir(path)
+            # Download the images using requests library
+            with open(os.path.join(path, "Anibis" + str(time.time()) + ".jpg"), "wb") as f:  # comment
+                f.write(requests.get(i).content)
+    except NoSuchElementException:
+        print("For apartment {}, 0 photos found at the website.".format(num))
+
+    time.sleep(0.5)
+print("Done with scraping from each link.\n\n")
+
+print(headers)
+print(details)
+print(descriptions)
+print(addresses)
+print(announcers)
+print(announces)
+print(tels)
