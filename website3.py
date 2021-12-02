be8007cbcebf3d7694d3c16045af3f0e7d733653
merry-mouse <80622089+merry-mouse@users.noreply.github.com>
2021-12-02 11:20:44 +0100

    merge (#6)
    * Combined 2 codes together now this program can also download a stack of photos from each apartment to the separate new folder (0,1,2... according to the link numerate in the database) to the "APARTMENTS" folder
    
    * Adding new comments that explain program behavior
    
    * Adding final comments
    
    * Combined 2 codes together now this program can also download a stack of photos from each apartment to the separate new folder (0,1,2... according to the link numerate in the database) to the "APARTMENTS" folder
    
    * Adding the first blocks of code
    
    * Added more blocks of code, scraped basic info, saved pics
    
    * Added more blocks of code, scraped basic info, saved pics
    
    * Added first block of code, checking if it works as suppose to

---
+++ website3.py
@@ -0,0 +1,20 @@
+# website for scraping: anibis.ch
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+import re
+import pandas as pd
+import os
+import requests
+import math
+# open the browser
+print("Openning browser...\n\n")
+
+# initialize automated browser window, specify the browser we want to use
+# store the path of the driver file in the brackets
+driver = webdriver.Chrome("C:/Users/potek/Jupyter_projects/chromedriver_win32/chromedriver.exe")
+# open the url you would like to request
+driver.get("https://www.anibis.ch/fr/c/immobilier-immobilier-locations?sct=GE")#comment
+time.sleep(4)
+# close cookies tab
+driver.find_element_by_class_name("cmp-closebutton_closeButton.cmp-closebutton_hasBorder").click()
