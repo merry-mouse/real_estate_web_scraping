6be4153cdf2eb1f5f7790dd0ac88f60617a5b512
anastasiia <potekh.anastasia@gmail.com>
2021-12-02 12:21:20 +0100

    Added the third block of code which scrapes links  from pages, and clicks to the next page when needs

--- website3.py
+++ website3.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os
 import requests
 import math
+
 # open the browser
 print("Openning browser...\n\n")
 
@@ -23,8 +24,22 @@ driver.find_element_by_class_name("cmp-closebutton_closeButton.cmp-closebutton_h
 # scraping the number of hits and pages found
 print("Checking how many apartments there are on the website...\n\n")
 num_hits_raw = driver.find_element_by_class_name("sc-1uujbw0-0.sc-1uujbw0-2.hnhhTs.jOORWi").get_attribute("innerText")
-num_hits = int((re.search("^(\d)'(\d+)", num_hits_raw).group(0)).replace("'",""))
+num_hits = int((re.search("^(\d)'(\d+)", num_hits_raw).group(0)).replace("'", ""))
 # there are 20 apartments per page, using math.ceil to find ceil number after division
-num_pages = math.ceil(num_hits/20)
+num_pages = math.ceil(num_hits / 20)
 print("There are {} items on {} pages found.\n\n".format(num_hits, num_pages))
 
+# scraping links (apartments) from each page
+links = []
+# end of range is EXCLUSIVE
+# to scrape ALL PAGES put num_pages+1 in the end of range
+for i in range(2, 4):
+    # scrapes links
+    containers = driver.find_elements_by_class_name("sc-gsTCUz.eJcPpT.sc-1gbeqqm-0.qNwID")
+    for apartment in containers:
+        link = apartment.get_attribute("href")
+        links.append(link)
+    print("{} links found.".format(len(links)))
+    # clicks to the next page
+    driver.find_element_by_xpath("//div[text()='{}']".format(i)).click()
+    time.sleep(2)
